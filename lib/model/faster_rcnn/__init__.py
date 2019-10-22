34360615d7e052be5cc774f18fec77df8b82445f
Alluka <13382092340@163.com>
2019-10-22 21:45:47 +0800

    conv3x3 pading

---
+++ lib/model/faster_rcnn/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/22 9:38 下午
+# @Author  : Sean Lee
+# @Email   : 13382092340@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
---
+++ lib/model/faster_rcnn/resnet.py
@@ -0,0 +1,30 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.autograd import Variable
+import math
+import torch.utils.model_zoo as model_zoo
+import pdb
+
+__all__ = ['ResNet', 'resnet18', 'resnet34', 'resnet50', 'resnet101',
+           'resnet152']
+
+
+model_urls = {
+    'resnet18': 'https://s3.amazonaws.com/pytorch/models/resnet18-5c106cde.pth',
+    'resnet34': 'https://s3.amazonaws.com/pytorch/models/resnet34-333f7ec4.pth',
+    'resnet50': 'https://s3.amazonaws.com/pytorch/models/resnet50-19c8e357.pth',
+    'resnet101': 'https://s3.amazonaws.com/pytorch/models/resnet101-5d3b4d8f.pth',
+    'resnet152': 'https://s3.amazonaws.com/pytorch/models/resnet152-b121ed2d.pth',
+}
+
+def conv3x3(in_planes, out_planes, stride=1):
+    '''
+    3x3 convolution with padding
+    '''
+    return nn.Conv2d(in_planes, out_planes, kernel_size=3, stride=stride,
+                     padding=1, bias=False)
