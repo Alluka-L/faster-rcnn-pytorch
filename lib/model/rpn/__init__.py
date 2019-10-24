4828895f83448656b50f3e863bdd2f562be4ab82
Alluka <13382092340@163.com>
2019-10-24 23:48:19 +0800

    part of generate anchors

---
+++ lib/model/rpn/__init__.py
@@ -0,0 +1,9 @@
+# !/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Time    : 2019/10/24 5:32 下午
+# @Author  : Sean Lee
+# @Email   : 13382092340@163.com
+# @File    : __init__.py.py
+# @Software: PyCharm
+
+
---
+++ lib/model/rpn/generate_anchors.py
@@ -0,0 +1,50 @@
+from __future__ import print_function
+import numpy as np
+import pdb
+
+
+def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
+                     scales=2**np.arange(3, 6)):
+    """
+    Generate anchor (reference) windows by enumerating aspect ratios X
+    scales wrt a reference (0, 0, 15, 15) window.
+    """
+    base_anchor = np. array([1, 1, base_size, base_size]) - 1
+    ratio_anchors = _ratio_enum(base_anchor, ratios)
+    pass
+
+
+def _whctrs(anchor):
+    """
+    :return: width, height, x center, and y center for an anchor (window).
+    """
+    w = anchor[2] - anchor[0] + 1
+    h = anchor[3] - anchor[1] + 1
+    x_ctr = anchor[0] + 0.5 * (w - 1)
+    y_ctr = anchor[0] + 0.5 * (h - 1)
+    return w, h, x_ctr, y_ctr
+
+
+def _mkanchors(ws, hs, x_ctr, y_ctr):
+    """
+    Given  a vector of width(ws) and height(hs) around a center
+    (x_ctr, y_ctr), output a set of anchors(windows).
+    """
+    ws = ws[:, np.newaxis]
+    hs = hs[:, np.newaxis]
+    anchors = np.hstack((x_ctr - 0.5 * (ws - 1),
+                         y_ctr - 0.5 * (hs - 1),
+                         x_ctr + 0.5 * (ws - 1),
+                         y_ctr + 0.5 * (ws - 1)))
+    return anchors
+
+
+def _ratio_enum(anchor, ratios):
+    """
+    Enumerate a set of anchors for each aspect ratio wrt an anchor.
+    """
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    size = w * h
+    size_ratios = size / ratios
+    ws = np.round(np.sqrt(size_ratios))
+    hs = np.round(ws * ratios)
