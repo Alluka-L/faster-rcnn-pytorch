7ff5418c2e7bb1dae735c66e91520b2aed2d9c6d
Alluka <13382092340@163.com>
2019-10-25 22:46:36 +0800

    generate_anchors is done

--- lib/model/rpn/generate_anchors.py
+++ lib/model/rpn/generate_anchors.py
@@ -11,7 +11,9 @@ def generate_anchors(base_size=16, ratios=[0.5, 1, 2],
     """
     base_anchor = np. array([1, 1, base_size, base_size]) - 1
     ratio_anchors = _ratio_enum(base_anchor, ratios)
-    pass
+    anchors = np.vstack([_scale_enum(ratio_anchors[i, :], scales)
+                        for i in range(ratio_anchors.shape[0])])
+    return anchors
 
 
 def _whctrs(anchor):
@@ -48,3 +50,26 @@ def _ratio_enum(anchor, ratios):
     size_ratios = size / ratios
     ws = np.round(np.sqrt(size_ratios))
     hs = np.round(ws * ratios)
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+
+def _scale_enum(anchor, scales):
+    """
+    Enumerate a set of anchors for each scale wrt an anchor.
+    """
+    w, h, x_ctr, y_ctr = _whctrs(anchor)
+    ws = w * scales
+    hs = h * scales
+    anchors = _mkanchors(ws, hs, x_ctr, y_ctr)
+    return anchors
+
+
+if __name__ == '__main__':
+    import time
+    t = time.time()
+    a = generate_anchors()
+    print(time.time() - t)
+    print(a)
+    from IPython import embed
+    embed()
