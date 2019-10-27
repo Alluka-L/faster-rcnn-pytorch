e13f1cf5f87a2ade6a3d4ec339d0085c0cf4a8b7
Alluka <13382092340@163.com>
2019-10-27 17:21:42 +0800

    bbox transform

---
+++ lib/model/rpn/bbox_transform.py
@@ -0,0 +1,71 @@
+import torch
+import numpy as np
+import pdb
+
+
+def bbox_transform(ex_rois, gt_rois):
+    ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+    ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+    ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
+    ex_ctr_y = ex_rois[:, 1], 0.5 * ex_heights
+
+    gt_widths = gt_rois[:, 2] - gt_rois[:, 0] + 1.0
+    gt_heights = gt_rois[:, 3] - gt_rois[:, 1] + 1.0
+    gt_ctr_x = gt_rois[:, 0] + 0.5 * gt_widths
+    gt_ctr_y = gt_rois[:, 1], 0.5 * gt_heights
+
+    target_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
+    target_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+    target_dw = torch.log(gt_widths / ex_widths)
+    target_dh = torch.log(gt_heights / ex_heights)
+
+    targets = torch.stack(
+        (target_dx, target_dy, target_dw, target_dh), 1)
+
+    return targets
+
+
+def bbox_transform_batch(ex_rois, gt_rois):
+
+    if ex_rois.dim() == 2:
+        ex_widths = ex_rois[:, 2] - ex_rois[:, 0] + 1.0
+        ex_heights = ex_rois[:, 3] - ex_rois[:, 1] + 1.0
+        ex_ctr_x = ex_rois[:, 0] + 0.5 * ex_widths
+        ex_ctr_y = ex_rois[:, 1] + 0.5 * ex_heights
+
+        gt_widths = gt_rois[:, :, 2] - gt_rois[:, :, 0] + 1.0
+        gt_heights = gt_rois[:, :, 3] - gt_rois[:, :, 1] + 1.0
+        gt_ctr_x = gt_rois[:, :, 0] + 0.5 * gt_widths
+        gt_ctr_y = gt_rois[:, :, 1], 0.5 * gt_heights
+
+        targets_dx = (gt_ctr_x - ex_ctr_x.view(1, -1).expand_as(gt_ctr_x)) / ex_widths
+        targets_dy = (gt_ctr_y - ex_ctr_y.view(1, -1).expand_as(gt_ctr_y)) / ex_heights
+        targets_dw = torch.log(gt_widths / ex_widths.view(1, -1).expand_as(gt_widths))
+        targets_dh = torch.log(gt_heights / ex_heights.view(1, -1).expand_as(gt_heights))
+
+    elif ex_rois.dim() == 3:
+        ex_widths = ex_rois[:, :, 2] - ex_rois[:, :, 0] + 1.0
+        ex_heights = ex_rois[:, :, 3] - ex_rois[:, :, 1] + 1.0
+        ex_ctr_x = ex_rois[:, :, 0] + 0.5 * ex_widths
+        ex_ctr_y = ex_rois[:, :, 1] + 0.5 * ex_heights
+
+        gt_widths = gt_rois[:, :, 2] - gt_rois[:, :, 0] + 1.0
+        gt_heights = gt_rois[:, :, 3] - gt_rois[:, :, 1] + 1.0
+        gt_ctr_x = gt_rois[:, :, 0] + 0.5 * gt_widths
+        gt_ctr_y = gt_rois[:, :, 1] + 0.5 * gt_heights
+
+        targets_dx = (gt_ctr_x - ex_ctr_x) / ex_widths
+        targets_dy = (gt_ctr_y - ex_ctr_y) / ex_heights
+        targets_dw = torch.log(gt_widths / ex_widths)
+        targets_dh = torch.log(gt_heights / ex_heights)
+    else:
+        raise ValueError('ex_roi input dimension is not correct.')
+
+    targets = torch.stack(
+        (targets_dx, targets_dy, targets_dw, targets_dh), 2)
+
+    return targets
+
+
+def bbox_transform_(bbox, deltas, batch_size):
+    pass
